4b897bbefdb5a994bc347e184d65686ddcee9549
Yash Talele <yashatsocials@gmail.com>
2021-11-19 17:09:32 +0530

    Update settings.py

--- telemedicine/settings.py
+++ telemedicine/settings.py
@@ -174,6 +174,6 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rahul.lele97@gmail.com'
+EMAIL_HOST_USER = ''
 #os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = 'yash@#97'
+EMAIL_HOST_PASSWORD = ''
